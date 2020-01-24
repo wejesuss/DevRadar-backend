fe518f7ed98f1cc4d865f5a02aba9ddb03b6d43f
Wemerson Jesus <wejesuss@gmail.com>
2020-01-24 09:28:26 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { MONGO_DB, MONGO_USER, MONGO_PASS } = require('../.env');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0-c8lyf.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+server.listen(3333);
